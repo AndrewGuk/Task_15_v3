5b803cad4bea0cd747a71e84112f0d7baa1f8d13
Andrei Huk <belakolala1@gmail.com>
2022-04-12 22:48:59 +0300

    Добавьте файлы проекта.

---
+++ Search_and_Archive/SearchAndArchive.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.IO.Compression;
+using System.Collections.Specialized;
+
+
+namespace Search_and_Archive
+{
+    public class SearchAndArchive
+    {
+        StringCollection log = new StringCollection();
+        public string NameSearchFile { get; set; }
+        public string DirectoryFile  { get; set; }
+        //принимающий файл:
+        // @"D:\floder\"
+        public SearchAndArchive(string NameSearchFile, string DirectoryFile)
+        {
+            this.NameSearchFile = NameSearchFile;
+            this.DirectoryFile = DirectoryFile;
+            Walk(new DirectoryInfo(DirectoryFile), NameSearchFile);
+        }
+
+        async void Walk(DirectoryInfo root, string search)
+        {
+            FileInfo[] files = null;
+            DirectoryInfo[] subDirs = null;
+
+            //Walk(new DirectoryInfo(path), search);
+            //Console.WriteLine("files block:");
+            //foreach (string item in log)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //получаем все файлыв текущем каталоге
+            try
+            {
+                files = root.GetFiles("*.*");
+                if (files.Length > 0)
+                {
+                    for (int i = 0; i < files.Length; i++)
+                    {
+                        if (files[i].Name.Contains(search))
+                        {
+                            ZipFile.CreateFromDirectory(files[0].DirectoryName, files[i].DirectoryName + ".zip");
+                            Console.WriteLine($"Done!\nFile {files[i].Name} is zipped!" +
+                                $"\nAdres : {files[i].Directory}");
+                            return;
+                        }
+                    }
+                    Console.WriteLine($"Sorry, i don't find file with name <{search}>");
+                }
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                log.Add(ex.Message);
+            }
+            catch (DirectoryNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            if (files != null)
+            {
+                //выводим имена папок в консоль
+                //foreach (FileInfo item in files)
+                //{
+                //    Console.WriteLine(item.FullName);
+                //}
+
+                //получаем все подкатегории
+                subDirs = root.GetDirectories();
+
+                //проходим по каждому каталогу
+                foreach (DirectoryInfo item in subDirs)
+                {
+                    //рекурсия
+                    Walk(item, search);
+                }
+            }
+        }
+    }
+}
---
+++ Task_15_v3_Console/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Search_and_Archive;
+
+namespace Task_15_v3
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Hello, please, enter the name of the file you want to archive:");
+            Console.ResetColor();
+            string findFile = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Please, enter the path to the folder:");
+            Console.ResetColor();
+            Console.WriteLine("For example: \"D:\\Floder\\...\\\" ");
+
+            string path = Console.ReadLine();    
+
+            var program = new SearchAndArchive(findFile, path);
+        }
+    }
+}
